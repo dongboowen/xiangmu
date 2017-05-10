233bcfee1f307af01d72cb8ad403e1302ca7cb25
e <905560820@qq.com>
2017-05-11 01:20:26 +0800

    第一次提交

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,13 @@
  * Created by Administrator on 2017/5/10 0010.
  */
 var gulp = require("gulp");
-gulp.task("show",function(){
-    console.log("hello gulp !");
-})
+var less = require("less");
+var aa   = require("gulp-load-plugins")();
+gulp.task('myless', function () {
+     gulp.src('app/styles/*.less')
+        .pipe(aa.less())
+        .pipe(gulp.dest('dist/css'));
+});
+gulp.task('dist',function () {
+    gulp.watch("app/styles/*.less",["myless"]);
+});
