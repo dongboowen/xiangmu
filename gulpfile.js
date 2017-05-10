7e2ea02c9a9bfe28104f4801f14e9b3524c7118a
dongboowen <360642226@qq.com>
2017-05-11 01:51:25 +0800

    dongwen ti jiao

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
 /**
  * Created by Administrator on 2017/5/10 0010.
  */
-var gulp = require("gulp");
-gulp.task("show",function(){
-    console.log("hello gulp !");
+//var gulp = require("gulp");
+//gulp.task("show",function(){
+//    console.log("hello gulp !");
+//})
+var gulp = require('gulp');
+var less =require("less");
+var plugins = require("gulp-load-plugins")();
+gulp.task("myless",function () {
+    gulp.src("app/scripts/*.less")
+        .pipe(plugins.less())
+        .pipe(gulp.dest("dist/css/"))
+})
+gulp.task("dist",function(){
+    gulp.watch("app/scripts/*.less",["myless"]);
 })
