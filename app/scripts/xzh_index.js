f19e2b331fef18190d5479cb74f7a1b7b7bd32f2
[xuzhihang] <[785921581@qq.com]>
2017-05-13 23:18:07 +0800

    主页基本完成

--- app/scripts/xzh_index.js
+++ app/scripts/xzh_index.js
@@ -14,3 +14,36 @@ function time_fun() {
         document.getElementById("mytime").innerText = two_char(h) + ":" + two_char(m) + ":" + two_char(s);
     }, 1000);
 }
+
+//搜索框
+function scroll() {
+    //判断当前的浏览器是否是IE9+ 或者其他浏览器
+    if (window.pageYOffset != null) {
+        return {
+            left: window.pageXOffset,
+            top: window.pageYOffset
+        };
+    }
+    //检测是否是怪异模式浏览器，就是没有声明<!DOCTYPE html>
+    else if(document.compatMode == "CSS1Compat"){
+        //正常模式
+        return {
+            left: document.documentElement.scrollLeft,
+            top: document.documentElement.scrollTop
+        };
+    }
+    //怪异浏览器
+    return {
+        left:document.body.scrollLeft,
+        top:document.body.scrollTop
+    };
+}
+document.onscroll = function () {
+    var dis = scroll().top;
+    var div = document.getElementById("top1");
+  if(dis>160) {
+        div.className = "top1 top5";
+    }else {
+        div.className = "top1";
+    }
+}
