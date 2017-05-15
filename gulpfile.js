8b94a7d681e1afc9f4dab4bef1badd965bc2ba9d
e <905560820@qq.com>
2017-05-15 15:08:49 +0800

    tijiao

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,6 @@
 /**
  * Created by Administrator on 2017/5/10 0010.
  */
-<<<<<<< HEAD
-//var gulp = require("gulp");
-//gulp.task("show",function(){
-//    console.log("hello gulp !");
-//})
-var gulp = require('gulp');
-var less =require("less");
-var plugins = require("gulp-load-plugins")();
-gulp.task("myless",function () {
-    gulp.src("app/scripts/*.less")
-        .pipe(plugins.less())
-        .pipe(gulp.dest("dist/css/"))
-})
-gulp.task("dist",function(){
-    gulp.watch("app/scripts/*.less",["myless"]);
-})
-=======
 var gulp = require("gulp");
 var less = require("less");
 var aa   = require("gulp-load-plugins")();
@@ -29,4 +12,3 @@ gulp.task('myless', function () {
 gulp.task('dist',function () {
     gulp.watch("app/styles/*.less",["myless"]);
 });
->>>>>>> 233bcfee1f307af01d72cb8ad403e1302ca7cb25
